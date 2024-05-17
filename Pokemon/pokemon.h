ee7f09cbf8f06063fcea19018edb984750e6f847
nzh0172 <nazirulhadi123@gmail.com>
2024-05-17 16:30:47 +0900

    Edit on Pokemon class

--- Pokemon/pokemon.h
+++ Pokemon/pokemon.h
@@ -9,7 +9,7 @@
 // Define a struct for a Pokemon
 class Pokemon {
 public:
-    std::string name;
+    std::string name; //need to put std::, using namespace std is bad practice idk
     int hp;
     int attack;
     Pokemon();
